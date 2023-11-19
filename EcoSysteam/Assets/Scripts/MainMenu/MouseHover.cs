c1a25d1bdaca4cc7bc866347ee0b6d0c268157e2
cheshire0 <seboklili1@gmail.com>
2023-11-19 21:07:28 +0100

    főmenü

---
+++ EcoSysteam/Assets/Scripts/MainMenu/MouseHover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+}
