e350ee4498ec04ec2a1094dca80b6ebe3194f21d
Bendoair <benedek.simon@gmail.com>
2023-11-23 19:47:49 +0100

    Uhum

--- EcoSysteam/Assets/Scripts/PlayerBehaviour.cs
+++ EcoSysteam/Assets/Scripts/PlayerBehaviour.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using System.Linq;
-using UnityEditor.Timeline.Actions;
 
 public class PlayerBehaviour : Synchronizable
 {
