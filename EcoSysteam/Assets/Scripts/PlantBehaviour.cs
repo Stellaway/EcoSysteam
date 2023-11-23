a1babde18c27b18af41d5d6abaf20ffb06d1f28d
cheshire0 <seboklili1@gmail.com>
2023-11-23 16:04:25 +0100

    plantbehaviour

--- EcoSysteam/Assets/Scripts/PlantBehaviour.cs
+++ EcoSysteam/Assets/Scripts/PlantBehaviour.cs
@@ -3,8 +3,29 @@ using UnityEngine;
 
 public class PlantBehaviour : Synchronizable
 {
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject myPrefab;
+
+    private float timePeriodTillFruit = 5000;
+    private float timeCounter = 0;
+
     // This method will be called every frame on the server side
-    protected override void ServerUpdate() {}
+    protected override void ServerUpdate()
+    {
+        if (timeCounter < timePeriodTillFruit)
+        {
+            timeCounter += Time.deltaTime;//ez így viszont sztem nem lesz fps-független, inkább a Time.deltaTime-t kéne összeadogatni (float), másodpercben van elvileg
+        }
+        else
+        {
+            //create fruit
+            // Instantiate at position (0, 0, 0) and zero rotation.
+            GameObject go = Instantiate(myPrefab, this.transform.position, Quaternion.identity);
+            go.GetComponent<NetworkObject>().Spawn(); // hogy mindenkinél megjelenjen
+
+            timeCounter = 0;
+        }
+    }
 
     // This is called only once I think
     public override void OnNetworkSpawn() {
