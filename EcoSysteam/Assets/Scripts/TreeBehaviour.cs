736c320c2b3faa3fa775ed75b6978d540b86e494
4321ba <nevenincs007@gmail.com>
2023-11-23 09:52:13 +0100

    fix treebehaviour networking part (hopefully)

--- EcoSysteam/Assets/Scripts/TreeBehaviour.cs
+++ EcoSysteam/Assets/Scripts/TreeBehaviour.cs
@@ -13,7 +13,22 @@ public class TreeBehaviour : PlantBehaviour
     private int timeCounter = 0;
 
     // This method will be called every frame on the server side
-    protected override void ServerUpdate() { }
+    protected override void ServerUpdate() {
+        base.ServerUpdate();
+        if(timeCounter < timePeriodTillFruit)
+        {
+            timeCounter++;//ez így viszont sztem nem lesz fps-független, inkább a delta-t kéne összeadogatni
+        }
+        else
+        {
+            //create fruit
+            // Instantiate at position (0, 0, 0) and zero rotation.
+            GameObject go = Instantiate(myPrefab, this.transform.position, Quaternion.identity);
+            go.GetComponent<NetworkObject>().Spawn(); // hogy mindenkinél megjelenjen
+
+            timeCounter = 0;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -24,17 +39,6 @@ public class TreeBehaviour : PlantBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(timeCounter < timePeriodTillFruit)
-        {
-            timeCounter++;
-        }
-        else
-        {
-            //create fruit
-            // Instantiate at position (0, 0, 0) and zero rotation.
-            Instantiate(myPrefab, this.transform.position, Quaternion.identity);
-
-            timeCounter = 0;
-        }
+        //TODO ezt inkább a ServerUpdate-ben, hogy csak a szerver spawnoljon, és szinkronban maradjon a kliensekkel
     }
 }
